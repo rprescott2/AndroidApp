dcd925d051d0e35b8f44c4c918467af70d0394e9
rprescott2 <48063028+rprescott2@users.noreply.github.com>
2019-03-13 21:24:22 +0300

    Добавлены новые активити. Создан интерфейс первичного окна. Создана проверка на совпадение слов. Не создан вывод об прохождении уровня

---
+++ app/src/main/java/com/example/androidapp/Level_1_Activity.java
@@ -0,0 +1,45 @@
+package com.example.androidapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class Level_1_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_1_);
+        ButtonLevel1Activity();
+    }
+    ArrayList<String> words = new ArrayList<>();
+    public void ButtonLevel1Activity(){
+        final int[] i = {0};
+        words.add("корова");
+        words.add("снег");
+        Button checkLevel = (Button)findViewById(R.id.Check_Level_1);
+        final EditText textLevel = (EditText)findViewById(R.id.Text_Level_1);
+        final int[] numberSucces = {(words.size())};
+        checkLevel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = textLevel.getText().toString();
+                    if(words.get(i[0]).equalsIgnoreCase(text)){
+                        Toast.makeText(Level_1_Activity.this, "Вы прошли уровень 1", Toast.LENGTH_SHORT).show();
+                    }
+                     else if (words.get(i[0]).equalsIgnoreCase(text)) {
+                        Toast.makeText(Level_1_Activity.this, "Вы угадали", Toast.LENGTH_SHORT).show();
+                        i[0]++;
+                    } else {
+                        Toast.makeText(Level_1_Activity.this, "Вы не угадали", Toast.LENGTH_SHORT).show();
+                    }
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/androidapp/MainActivity.java
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,70 +9,63 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity {
-    String ip = null;
-    Socket clientSocket = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            clientSocket = new Socket(ip, 9999);
-            ip = new GetIpAdress().run2().replaceFirst("/", "");
             ButtonActivity();
-        }catch (IOException e){
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
     public void ButtonActivity(){
-        final int entrychecked = 0;
-        Button entryButton = (Button)findViewById(R.id.EntryButton);
-        final EditText PoleLogin = (EditText)findViewById(R.id.Login);
-        final EditText PolePassword = (EditText)findViewById(R.id.Password);
-        final CheckBox CheckPolePassword = (CheckBox)findViewById(R.id.PasswordCheck);
-        CheckPolePassword.setOnClickListener(new View.OnClickListener() {
+        Button level1 = (Button)findViewById(R.id.Level1);
+        Button level2 = (Button)findViewById(R.id.Level2);
+        Button level3 = (Button)findViewById(R.id.Level3);
+        Button level4 = (Button)findViewById(R.id.Level4);
+        Button level5 = (Button)findViewById(R.id.Level5);
+        Button level6 = (Button)findViewById(R.id.Level6);
+        level1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(CheckPolePassword.isChecked() == true){
-                    PolePassword.setInputType(9);
-                } else {
-                    PolePassword.setInputType(129);
-                }
+                Intent shift = new Intent(".Level_1_Activity");
+                startActivity(shift);
             }
         });
-        entryButton.setOnClickListener(new View.OnClickListener() {
+        level2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String indexEntery = null;
-                try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                   BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-                out.write( PoleLogin.getText().toString() +  "\n");
-                out.flush();
-                    out.write( PolePassword.getText().toString() +  "\n");
-                    out.flush();
-                    indexEntery = reader.readLine();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                if(indexEntery.contains("true") == true) {
-                    Toast.makeText(MainActivity.this, "Вы вошли под учетной записью " + PoleLogin.getText().toString(), Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "False", Toast.LENGTH_LONG).show();
-                }
+                Intent shift = new Intent(".Level_2_Activity");
+                startActivity(shift);
+            }
+        });
+        level3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shift = new Intent(".Level_3_Activity");
+                startActivity(shift);
+            }
+        });
+        level4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shift = new Intent(".Level_4_Activity");
+                startActivity(shift);
+            }
+        });
+        level5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shift = new Intent(".Level_5_Activity");
+                startActivity(shift);
+            }
+        });
+        level6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shift = new Intent(".Level_6_Activity");
+                startActivity(shift);
             }
         });
     }
-    }
+}
